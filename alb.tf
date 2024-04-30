b9c7f6cdbf829fe5b955fb2abdd1949ffe6f6a83
Shubhashri Sinha <shubhi@Shubhashris-MacBook-Air-2.local>
2024-04-30 16:35:58 +0530

    added alb

--- alb.tf
+++ alb.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "nginx" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.nginx.id]
   subnets            = [aws_subnet.public.id, aws_subnet.public.id]
+  enable_cross_zone_load_balancing = true
 
   enable_deletion_protection = true
 
@@ -16,4 +17,39 @@ resource "aws_lb" "nginx" {
   tags = {
     Environment = "Dev"
   }
+}
+
+resource "aws_lb_target_group" "nginx_a" { 
+ name     = "nginx-target-group"
+ port     = 80
+ protocol = "HTTP"
+ vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_target_group_attachment" "nginx_tg_attachment_a" {
+ count = length(aws_instance.nginx)
+ target_group_arn = aws_lb_target_group.nginx_a.arn
+ target_id        = aws_instance.nginx[count.index].id
+ port             = 80
+}
+
+resource "aws_lb_listener" "nginx" {
+  load_balancer_arn = aws_lb.nginx.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn" //cert goes here
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.nginx_a.arn
+ }
+
+  depends_on = [
+    aws_lb.nginx
+  ]
+
+  lifecycle {
+      create_before_destroy = false
+    }
 }
